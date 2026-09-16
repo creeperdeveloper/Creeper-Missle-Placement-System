1c34362f958f00e2be246b5bc92e5dacc7ded523
creeper_developers <shuukiuesugi1@gmail.com>
2026-09-16 18:51:21 +0900

    Refactor block setting logic in block_controller.lua

--- block_controller.lua
+++ block_controller.lua
@@ -1,58 +1,9 @@
-if type(commands) ~= "table"
-    or type(commands.setblock) ~= "function" then
-
-    term.setBackgroundColor(colors.white)
-    term.setTextColor(colors.black)
-    term.clear()
-
-    local width, height = term.getSize()
-    local message = "SYSTEM LOCKED"
-
-    term.setCursorPos(
-        math.floor((width - #message) / 2) + 1,
-        math.floor(height / 2)
-    )
-
-    term.write(message)
-
-    while true do
-        os.pullEventRaw()
-    end
-end
-
-local config = dofile("/config.lua")
-
-local sides = {
-    "left",
-    "right",
-    "front",
-    "back",
-    "top",
-    "bottom"
-}
-
 while true do
-    for _, side in ipairs(sides) do
-        local signal = redstone.getAnalogInput(side)
+    if redstone.getAnalogInput("left") > 0 then
+        commands.setblock("~", "~-1", "~", "minecraft:stone")
 
-        if signal > 0 then
-            local block = config[side]
-
-            if type(block) == "string"
-                and block ~= ""
-                and block ~= "minecraft:air" then
-
-                commands.setblock(
-                    "~",
-                    "~-1",
-                    "~",
-                    block
-                )
-
-                while redstone.getAnalogInput(side) > 0 do
-                    sleep(0.05)
-                end
-            end
+        while redstone.getAnalogInput("left") > 0 do
+            sleep(0.05)
         end
     end
 
